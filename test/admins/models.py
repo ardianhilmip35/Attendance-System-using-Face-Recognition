6d2dab7e1cd719e6cb9a0adf7cf59a3f58ee92f5
Galuh304 <galuhapriliano30@gmail.com>
2022-11-14 20:04:31 +0700

    galuh commit

--- test/admins/models.py
+++ test/admins/models.py
@@ -4,9 +4,14 @@ from django.db import models
 
 
 class Member(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
+    Nim = models.CharField(max_length=50)
+    Nama = models.CharField(max_length=50)
+    Kelas = models.CharField(max_length=50)
+    Semester = models.CharField(max_length=50)
+    Telepon = models.CharField(max_length=50)
+    Alamat = models.CharField(max_length=50)
+    JenisKelamin = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.firstname + " " + self.lastname
+    # def __str__(self):
+    #     return self.firstname + " " + self.lastname
+    #create model?
